23c537f9399a7cff8095ebd08976eeabbfc322d5
Aman Raj <amanr0883@gmail.com>
2024-08-16 14:55:08 +0530

    BookifyApp

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './home/Home'
-import {Route,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Courses from './Courses/Courses'
 import Contacts from './Contacts/Contacts'
 import Abouts from './Abouts/Abouts'
@@ -9,15 +9,17 @@ import SignUp from './Components/SignUp'
 const App = () => {
   return (
     <>
-    <div className='bg-white min-h-screen '>
-      <Routes>
-         <Route path='/' element={<Home/>}/>
-         <Route path='/Course' element={<Courses/>}/>
-         <Route path='/Contact' element={<Contacts/>}/>
-         <Route path='/About' element={<Abouts/>}/>
-         <Route path='/Sign Up' element={<SignUp/>}/>
-      </Routes>  
-    </div>
+      <div className='bg-white min-h-screen '>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Course' element={<Courses />} />
+            <Route path='/Contact' element={<Contacts />} />
+            <Route path='/About' element={<Abouts />} />
+            <Route path='/Sign Up' element={<SignUp />} />
+          </Routes>
+
+      </div>
     </>
   )
 }
